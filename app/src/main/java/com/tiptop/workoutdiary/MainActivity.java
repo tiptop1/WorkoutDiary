3da995f23578e8c7a93a0359625020d0362b7695
tiptop <Tomasz.Gasior@gmail.com>
2017-11-25 23:48:10 +0100

    Added fake support for fragments switcy.

--- app/src/main/java/com/tiptop/workoutdiary/MainActivity.java
+++ app/src/main/java/com/tiptop/workoutdiary/MainActivity.java
@@ -1,7 +1,13 @@
 package com.tiptop.workoutdiary;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigation);
+        final FragmentManager fragmentManager = getSupportFragmentManager();
+        final Fragment fragment = null;
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id){
+                    case R.id.action_places:
+//                        fragment = new PlacesFragment();
+                        break;
+                    case R.id.action_exercises:
+//                        fragment = new ExcercisesFragment();
+                        break;
+                    case R.id.action_workouts:
+//                        fragment = new WorkoutsFragment();
+                        break;
+                    case R.id.action_statistics:
+//                        fragment = new StatisticsFragment();
+                        break;
+                }
+                final FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.main_container, fragment).commit();
+                return true;
+            }
+        });
     }
 }
