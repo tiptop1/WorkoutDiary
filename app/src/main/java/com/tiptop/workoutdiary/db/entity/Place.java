19b2a380410ea868a88746515649477287483200
tiptop <Tomasz.Gasior@gmail.com>
2018-03-06 06:57:32 +0100

    Added unique index on "name" column. Made "name" column @NonNull.

--- app/src/main/java/com/tiptop/workoutdiary/db/entity/Place.java
+++ app/src/main/java/com/tiptop/workoutdiary/db/entity/Place.java
@@ -2,18 +2,21 @@ package com.tiptop.workoutdiary.db.entity;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
 /**
  * Created by tiptop on 24.01.18.
  */
 
-@Entity(tableName = "places")
+@Entity(tableName = "places", indices={@Index(value="name", unique=true)})
 public class Place {
     @PrimaryKey(autoGenerate = true)
     public long id;
 
     @ColumnInfo
+    @NonNull
     public String name;
 
     @ColumnInfo
