8958d1b0f5763e1201635ef779133a1b1b774adf
tiptop <Tomasz.Gasior@gmail.com>
2018-03-06 06:58:49 +0100

    Added workoutDao() method.

--- app/src/main/java/com/tiptop/workoutdiary/db/WorkoutDiaryDb.java
+++ app/src/main/java/com/tiptop/workoutdiary/db/WorkoutDiaryDb.java
@@ -4,13 +4,16 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
 import com.tiptop.workoutdiary.db.dao.PlacesDao;
+import com.tiptop.workoutdiary.db.dao.WorkoutDao;
 import com.tiptop.workoutdiary.db.entity.Place;
+import com.tiptop.workoutdiary.db.entity.Workout;
 
 /**
  * Created by tiptop on 24.01.18.
  */
 
-@Database(version = 1, entities = {Place.class}, exportSchema = false)
+@Database(entities = {Place.class, Workout.class}, exportSchema = false, version = 1)
 public abstract class WorkoutDiaryDb extends RoomDatabase {
     public abstract PlacesDao placesDao();
+    public abstract WorkoutDao workoutDao();
 }
